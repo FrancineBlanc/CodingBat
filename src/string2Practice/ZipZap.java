4bd132d9f6dba9b7d26faba351bb5fd023ee1941
FrancineBlanc <francine.blanc@gmail.com>
2022-08-30 18:34:58 +0100

    Add correct solution for ZipZap

--- src/string2Practice/ZipZap.java
+++ src/string2Practice/ZipZap.java
@@ -9,18 +9,19 @@ zipZap("zzzopzop") → "zzzpzp"
 public class ZipZap {
     public static void main(String[] args) {
         System.out.println(zipZap("zopzop"));
+        System.out.println(zipZap("zipXzap"));
     }
 
     public static String zipZap(String str) {
-        StringBuilder stringBuilder = new StringBuilder();
-        for(int i = 1; i < str.length() - 1; i++) {
-            if (str.charAt(i - 1) == 'z' && str.charAt(i + 1) == 'p') {
-                stringBuilder.append(str.charAt(i - 1)).append(str.charAt(i + 1));
+        if (str.length() < 2) return str;
+        String newStr = str.substring(0, 1);
+
+        for (int i = 1; i < (str.length() - 1); i++) {
+            if ((str.charAt(i - 1) != 'z') || (str.charAt(i + 1) != 'p')) {
+                newStr += str.charAt(i);
             }
-//            } else if (str.charAt(i - 1) != 'z' && str.charAt(i + 1) != 'p') {
-//                stringBuilder.append(str.charAt(i));
-//            }
         }
-        return stringBuilder.toString();
+        newStr += str.substring(str.length() - 1);
+        return newStr;
     }
 }
