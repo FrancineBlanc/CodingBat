37fd5e7ed48bc8931488ded6e6ce1d3fc9eb563d
FrancineBlanc <francine.blanc@gmail.com>
2022-09-28 13:08:25 +0100

    upload correctly solved plusout and xyzmiddle problems

--- src/string2Practice/PlusOut.java
+++ src/string2Practice/PlusOut.java
@@ -1,14 +1,25 @@
 package string2Practice;
 
+/*Given a string and a non-empty word string, return a version of the original String where all chars have been replaced
+by pluses ("+"), except for appearances of the word string which are preserved unchanged.
+
+
+plusOut("12xy34", "xy") → "++xy++"
+plusOut("12xy34", "1") → "1+++++"
+plusOut("12xy34xyabcxy", "xy") → "++xy++xy+++xy"
+*/
+
 public class PlusOut {
     public static void main(String[] args) {
-        System.out.println(plusOut("12xy34", "xy"));
-        System.out.println(plusOut("12xy34", "1"));
-        System.out.println(plusOut("12xy34xyabcxy", "xy"));
-        System.out.println(plusOut("abXYabcXYZ", "abc"));
+//        System.out.println(plusOut("12xy34", "xy"));
+//        System.out.println(plusOut("12xy34", "1"));
+//        System.out.println(plusOut("12xy34xyabcxy", "xy"));
+        System.out.println(plusOut("--++ab", "++"));
     }
 
     public static String plusOut(String str, String word) {
-        return "";
+        word = java.util.regex.Pattern.quote(word);
+        return str.replaceAll("\\G((?:" + word + ")*+).", "$1+");
     }
+
 }
--- src/string2Practice/XyzMiddle.java
+++ src/string2Practice/XyzMiddle.java
@@ -1,4 +1,5 @@
 package string2Practice;
+
 /*
 Given a string, does "xyz" appear in the middle of the string? To define middle, we'll say that the number of chars to
 the left and right of the "xyz" must differ by at most one. This problem is harder than it looks.
@@ -11,21 +12,15 @@ public class XyzMiddle {
     public static void main(String[] args) {
         System.out.println(xyzMiddle("AAxyzBB"));
         System.out.println(xyzMiddle("AxyzBB"));
-        System.out.println(xyzMiddle("AxyzBBB"));
+        System.out.println(xyzMiddle("AAAxyzB"));
     }
 
     public static boolean xyzMiddle(String str) {
-        if (str.length() <= 2) return false;
-        int indexXyz = str.lastIndexOf("xyz");
-        String precedingSubstring = str.substring(0, indexXyz);
-        String followingSubstring = str.substring(indexXyz + 3);
+        if (str.length() < 3 || !str.contains("xyz")) return false;
 
-        if (precedingSubstring.length() >= followingSubstring.length()) {
-            return precedingSubstring.length() - followingSubstring.length() <= 1;
-        }
-
-        if (followingSubstring.length() >= precedingSubstring.length()) {
-            return followingSubstring.length() - precedingSubstring.length() <= 1;
+        int mid = (str.length() / 2) - 1;
+        if (str.substring(mid, mid + 3).equals("xyz") || (str.length() % 2 == 0 && str.substring(mid - 1, mid + 2).equals("xyz"))) {
+            return true;
         }
         return false;
     }
